befaaf477d8b1f347a122586cdb948b6cf0adc2c
StebivkaYrii <markuius54621490@gmail.com>
2022-05-16 21:12:40 +0300

    Hw1

---
+++ src/app/constants/urls.ts
@@ -0,0 +1,8 @@
+import {environment} from "../../environments/environment";
+
+const {API:baseURL} = environment;
+export const urls = {
+  users: `${baseURL}users`,
+  posts: `${baseURL}/posts`,
+  comments:` ${baseURL}/comments`
+}
